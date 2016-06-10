0f7cc16b6b16d255deb77ececcfbb6433b3f9bd4
mlabouardy <mohamed@labouardy.com>
2016-06-10 16:37:09 +0200

    clean archives

--- bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## Get themes
-
-## Get plugins
-
-## Build environment
-
-## Init database
--- load-backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-WORDPRESS_DB_NAME='blog';
-WORDPRESS_DB_USER='root';
-WORDPRESS_DB_PASSWORD='root';
-container=$(docker ps -q --filter="name=blog_db");
-filename='backup.sql';
-docker exec $container sh -c "exec mysql -u $WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD $WORDPRESS_DB_NAME" < $filename
