ca5e7d74476e8de7bb2047a44990a9d955632cc0
LABOUARDY Mohamed <mohamed.labouardy@gmail.com>
2016-06-10 16:50:43 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-## Get themes
-
-## Get plugins
+## Get themes & Plugins
+echo "Get Blog themes and plugins";
+wget https://github.com/mlabouardy/blog-android/archive/archives.zip
+unzip archives.zip
+cp -r blog-android-archives/themes themes
+cp -r blog-android-archives/plugins plugins
+rm -rf archives.zip blog-android-archives
 
 ## Build environment
+echo "Create docker containers";
+docker-compose up -d --force-recreate
 
 ## Init database
