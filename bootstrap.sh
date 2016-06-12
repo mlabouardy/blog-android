f6d44205350c6d8858ed4dbae74a5e623c217997
mlabouardy <mohamed.labouardy@gmail.com>
2016-06-12 19:10:53 +0200

    load database with dump

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
+WORDPRESS_DB_NAME='blog_db';
+WORDPRESS_DB_USER='root';
+WORDPRESS_DB_PASSWORD='root';
+SERVER_URL='https://github.com/mlabouardy/blog-android/archive';
+LOG_FILE='bootstrap.log';
+
 ## Get themes & Plugins
-echo "Get Blog themes and plugins";
-wget https://github.com/mlabouardy/blog-android/archive/archives.zip
-unzip archives.zip
+echo 'Get Blog themes and plugins';
+wget -q $SERVER_URL/archives.zip;
+unzip archives.zip >> $LOG_FILE;
 cp -r blog-android-archives/themes themes
 cp -r blog-android-archives/plugins plugins
 rm -rf archives.zip blog-android-archives
 
 ## Build environment
-echo "Create docker containers";
-docker-compose up -d --force-recreate
+echo 'Create docker containers';
+docker-compose up -d --force-recreate >> $LOG_FILE;
 
 ## Init database
-echo "Initialize database";
-wget https://github.com/mlabouardy/blog-android/backup/backup.zip
-unzip backup.zip
-mv blog-android-archives/backup.sql backup.sql
-# mysql -u < backup.sql
+echo 'Initialize database';
+wget -q $SERVER_URL/backup.zip;
+unzip backup.zip >> $LOG_FILE;
+sleep 5
+docker exec -i $WORDPRESS_DB_NAME mysql -u $WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD blog < blog-android-backup/backup.sql >> $LOG_FILE;
 rm -rf backup.zip blog-android-backup
+
+echo 'Enjoy :)';
